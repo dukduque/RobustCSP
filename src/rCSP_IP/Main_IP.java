10f26ca73fa076551f98c6da5ac8860d6aee8fa8
dukduque <d.duque25@uniandes.edu.co>
2015-01-20 12:21:38 -0500

    RCSP instances

--- src/rCSP_IP/Main_IP.java
+++ src/rCSP_IP/Main_IP.java
@@ -16,6 +16,39 @@ public class Main_IP {
 	
 
 	public static void main(String[] args) throws InterruptedException {
+//		runToy();
+		runRCSP();
+//		runNJ();
+	}
+	
+	public static void runRCSP() {
+		try {
+
+			int num_weights = 0;
+			String ini;
+			for (int ins = 1; ins <= 1; ins++) {
+
+				ini = "ini/RCSP/rcsp5_3_1.ini";
+			
+				Settings instance = new Settings(ini);
+				num_weights = instance.numScenarios + instance.numCtrs;
+				int numSPPs = num_weights;
+				IP_DH data = new IP_DH(instance, instance.numScenarios,
+						instance.numCtrs);
+				data.ReadC();
+				System.out.print("Pulse_R-CSP: " + ini + "\t s:"
+						+ instance.source + "e:" + instance.sink);
+				OptModel IP = new OptModel(data);
+			}
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	public static void runToy(){
 		
 		try {
 			
@@ -25,6 +58,31 @@ public class Main_IP {
 				
 //				ini = "ini/Rob_20_NJ"+ins+".ini";
 				ini = "ini/toy"+ins+".ini";
+				Settings instance = new Settings(ini);
+				num_weights = instance.numScenarios + instance.numCtrs;
+				int numSPPs = num_weights;
+				IP_DH data = new IP_DH(instance, instance.numScenarios, instance.numCtrs);
+				data.ReadC();
+				System.out.print("Pulse_R-CSP: " + ini + "\t s:" + instance.source + "e:" + instance.sink);
+				OptModel IP = new OptModel(data);
+			}
+				
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+public static void runNJ(){
+		
+		try {
+			
+			int num_weights =0;
+			String ini;
+			for (int ins = 1; ins <=1; ins++) {
+				
+				ini = "ini/Rob_20_NJ"+ins+".ini";
+				//ini = "ini/toy"+ins+".ini";
 				Settings instance = new Settings(ini);
 				num_weights = instance.numScenarios + instance.numCtrs;
 				int numSPPs = num_weights;
